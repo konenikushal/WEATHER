93ac62c80846bf996e54a42524ac6204595f980c
kfgosline <kgosline@uncc.edu>
2024-04-01 12:46:14 -0400

    Icons only on render

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -12,25 +12,26 @@ import { useState, useEffect } from "react";
 
 function WeatherCard({data}) {
     const[w_icon, setW_icon] = useState(null);
-    const iconMapping = {
-        "01d": clear_icon,
-        "01n": clear_icon,
-        "02d": cloud_icon,
-        "02n": cloud_icon,
-        "03d": drizzle_icon,
-        "03n": drizzle_icon,
-        "04d": drizzle_icon,
-        "04n": drizzle_icon,
-        "09d": rain_icon,
-        "09n": rain_icon,
-        "10d": rain_icon,
-        "10n": rain_icon,
-        "11d": rain_icon,
-        "11n": rain_icon,
-        "13d": snow_icon,
-        "13n": snow_icon,};
-
+   
     useEffect(() => {
+        const iconMapping = {
+            "01d": clear_icon,
+            "01n": clear_icon,
+            "02d": cloud_icon,
+            "02n": cloud_icon,
+            "03d": drizzle_icon,
+            "03n": drizzle_icon,
+            "04d": drizzle_icon,
+            "04n": drizzle_icon,
+            "09d": rain_icon,
+            "09n": rain_icon,
+            "10d": rain_icon,
+            "10n": rain_icon,
+            "11d": rain_icon,
+            "11n": rain_icon,
+            "13d": snow_icon,
+            "13n": snow_icon,};
+    
         // Check if data exists and if the icon code is in the mapping
         if (data && data.weather && data.weather[0] && data.weather[0].icon) {
             const iconCode = data.weather[0].icon;
@@ -42,7 +43,7 @@ function WeatherCard({data}) {
                 console.error("Unknown weather icon code:", iconCode);
             }
         }
-    }, [data, iconMapping]);
+    }, [data]);
 
     console.log(data)
     return (
